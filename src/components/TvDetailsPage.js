0692cde83eef81da59a5ebd347c2e307c22623c3
Ankit Shahu <ankitshahu2023@gmail.com>
2024-07-03 00:34:26 +0530

    update react redux

--- src/components/TvDetailsPage.js
+++ src/components/TvDetailsPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-import img from './mmm.jpg';
 import db from "../appwrite/database";
 import { toast } from 'react-toastify';
 import { Form, Button } from 'react-bootstrap';
